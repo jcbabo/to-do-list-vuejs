6698fb8734ee900db892309d14d8045ff0761eb5
Jonathan Babo <jbabo@protonmail.com>
2022-06-15 23:45:35 -0300

    ajustes iniciais

--- README.md
+++ README.md
@@ -1,29 +1,20 @@
 # to-do-list-vuejs
 
-## Project setup
-```
-yarn install
-```
+A ideia aqui é repetir a dose com o ToDo list, mas agora usando o framework VueJs 3, que é o framework que uso no trabalho atualmente e quero me aprimorar no entendimento dele.
 
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
+Tenho algumas outras ideias de projetos em mente que iniciarei o desenvolvimento logo quando finalizar esse ToDo list aqui. Por hora, somente desenvolvimento frontend, aprimorando meus conhecimentos em Javascript e no VueJs 3. Depois devo partir para o desenvolvimento backend (provavelmente em C#, inicialmente, pois é o que uso no trabalho). Mas por hora, por aqui haverá apenas frontend applications.
 
-### Compiles and minifies for production
-```
-yarn build
-```
+Sobre essa ToDo list, não há nada de novo o que mostrar de diferente da outra que fiz em javascript puro. Vou "apenas" componentizar algumas coisas para treinar com o VueJs 3, css, estilo mobile first, entre outras coisas.
 
-### Run your unit tests
-```
-yarn test:unit
-```
+A base será a mesma: uma caixa de texto com um input, um botão para inserir a tarefa, a lista de tarefas e suas funções.
 
-### Lints and fixes files
-```
-yarn lint
-```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+====================================================================================================================
+
+Pretendo sempre ir explicando o avanço da aplicação por aqui.
+
+Nesse primeiro commit de desenvolvimento da lógica, implementei os métodos para criação e apagamento de tarefas, para dar a tarefa como concluída e pus o vínculo com o botão "Inserir" e tecla "Enter" para adicionar tarefa.
+
+Pretendo, nos próximos passos:
+- mostrar a tarefa inserida na tela
+- criar componentes com determinadas partes da aplicação e abstrair suas lógicas para arquivos independentes, limpando o código do arquivo inicial e melhorando a organização dos arquivos do projeto.
--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,160 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app-container">
+    <component :is="layout" class="dynamic-layout">
+      <router-view />
+    </component>
+  </div>
+  <div class="page">
+    <div class="principal-content">
+      <div class="title-page">
+        <h1>To-do List</h1>
+        <h5>Agora em VueJs!</h5>
+      </div>
+      <div class="todo-input">
+        <input type="text" name="todo-input" v-model="todo" @keyup.enter="addTask(todo)" />
+        <button class="btn-input" @click="addTask(todo)">Inserir
+        </button>
+      </div>
+      <div class="task-grid">
+        <input type="checkbox" />
+        <ul>
+          <li>As tarefas aparecerão aqui</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component'
-import HelloWorld from './components/HelloWorld.vue'
+<script>
 
-@Options({
-  components: {
-    HelloWorld
+export default {
+  name: 'Todo-List',
+
+  created() {
+
+    document.title = 'Application To-do List'
+
+  },
+
+  data() {
+    return {
+      id: 0, //id das tarefas listadas
+      todo: '', // nome da tarefa que será vinculada ao input
+      todos: [], // array vazio que armazenará as tarefas
+    }
+  },
+
+  methods: {
+    //gera uma id novo para cada tarefa
+    genId() {
+      return (this.id += 1); //incrementa o data property "id", iniciando no '1'
+    },
+
+    //adiciona uma nova tarefa ao array 'todos'
+    addTask(todo) {
+      this.todos.push({
+        id: this.genId(), //gera um novo id
+        name: todo, //o nome da tarefa
+        done: false, //para que a tarefa apareça como 'não completada' na lista
+      });
+      this.todo = ""; //limpa os dados do input
+    },
+
+    //pega o índice da tarefa corrente para facilita uma explusão posterior
+    getIndex(todo) {
+      //a variável recebe a tarefa como parâmetro, procura seu índice e retorna ele
+      let index = this.todos.findIndex(item => item.id === todo.id);
+      return index;
+    },
+
+    //exclui uma tarefa a partir de seu índice atraveś de um splice no array
+    removeTask(todo) {
+      let index = this.getIndex(todo); //a variável recebe o índice da tarefa em questão
+      this.todos.splice(index, 1); //faz um splice no array e remove o índice da variável "index"
+    },
+
+    //recebe uma tarefa como parâmetro de entrada e muda seu status
+    taskComplete(todo) {
+      todo.done = !todo.done
+    },
   }
-})
-export default class App extends Vue {}
+}
+
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+// .page {
+//   display: grid;
+//   justify-content: center;
+//   background-color: $bg-page;
+//   margin: auto;
+//   border: $borders 2px solid;
+//   border-radius: 10px;
+//   max-width: 750px;
+// }
+
+// .principal-content {
+//   padding: 20px 10px;
+// }
+
+// .title-page {
+//   color: $text;
+//   text-align: center;
+// }
+
+// h1, h5 {
+//   margin: 0px;
+//   padding-bottom: 14px;
+// }
+
+// .todo-input {
+//   display: grid;
+//   grid-template-columns: 1fr 1fr;
+//   width: fit-content;
+//   column-gap: 10px;
+//   padding: 30px 0px;
+// }
+
+// input, button {
+//   color: $text;
+//   background-color: $bg-input;
+//   border: $borders  1px solid;
+//   border-radius: 3px;
+//   box-shadow: none;
+//   padding: 5px 10px;
+// }
+
+// .btn-input {
+//   width: 60px;
+// }
+
+// .task-grid {
+//   display: grid;
+//   grid-template-columns: auto 1fr;
+//   column-gap: 5px;
+// }
+
+// ul {
+//   margin: 0px;
+//   list-style-type: none;
+//   padding: 0px;
+// }
+
+// li {
+//   color: $text;
+// }
+
+:root {
+  font-size: 62.5%;
+  font-family: 'Prompt', Sans-Serif;
+
+  body {
+    font-size: 1.7rem;
+  }
+
+  #app,
+  .app-container {
+    height: 100vh;
+  }
 }
 </style>
